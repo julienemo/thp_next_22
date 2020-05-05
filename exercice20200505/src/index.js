7b29adb6a2a40f2f154d97ed13a0b56b1b9bf33d
julienemo <juliette.nada@gmail.com>
2020-05-05 11:23:24 +0200

    [create] exercice app - installed node-sass and bootstrap - CSS relation tested ok

---
+++ exercice20200505/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.scss";
+
+ReactDOM.render(
+  <button className="btn btn-outline-warning">hello</button>,
+  document.getElementById("root")
+);
